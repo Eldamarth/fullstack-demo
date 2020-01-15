2d07cd19becb177fa3e02882b83893a95c0f875c
James Kelley <james.a.kelley.mars@gmail.com>
2020-01-15 11:25:20 -0700

    Create basic routes in index.js, create db connection, add mongoose-auto-increment plugin

--- server/db/queries.js
+++ server/db/queries.js
@@ -1,1 +1,26 @@
 // create db
+const mongoose = require ('mongoose');
+const autoIncrement = require ('mongoose-auto-increment');
+
+
+mongoose.connect('mongodb://localhost:27017', {useNewUrlParser: true});
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log("Connected to DB!")
+});
+
+const db = mongoose.connection;
+
+autoIncrement.initialize(db);
+
+const bugSchema = new mongoose.Schema({
+    bugName: '1',
+    bugDescription: 'Async Swim needs to be fixed, desperately.',
+    reportedBy: 'Bailey',
+    createdDate: '1/5/2020',
+    assignedTo: 'Teddi',
+    threatLevel: 'Critical',
+})
+
+bugSchema.plugin(autoIncrement.plugin, 'Bug');
+const Bug = db.model('Bug', bugSchema);
--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
 const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 3000;
+
+app.use(bodyParser.json())
+
+app.get('/bugs', (req,res)=>{
+    console.log("bugs requested");
+    res.sendStatus(200);
+});
+app.post('/bugs', (req, res)=>{
+    console.log("Bug post received");
+    res.sendStatus(200);
+});
+
+//server.listen
+app.listen(port, ()=>{
+    console.log("Listening on port "+port);
+})
