3a91e7016b8e3102f5dd206c223d8e6c7360758d
James Kelley <james.a.kelley.mars@gmail.com>
2020-01-15 14:26:22 -0700

    Create basic component for submitting bugs

---
+++ src/components/SubmitDialog.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class SubmitDialog extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {
+
+    }
+
+    render() {
+        return (<div>
+
+        </div>)
+    }
+}
+
+export default SubmitDialog;
