51a8376b6ecd24660f5490494366f3fd07fc4f91
James Kelley <james.a.kelley.mars@gmail.com>
2020-01-15 12:30:53 -0700

    Get routes working, returning data posted

--- server/db/queries.js
+++ server/db/queries.js
@@ -1,26 +1,38 @@
 // create db
 const mongoose = require ('mongoose');
 const autoIncrement = require ('mongoose-auto-increment');
-
-
 mongoose.connect('mongodb://localhost:27017', {useNewUrlParser: true});
+const db = mongoose.connection;
+
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
   console.log("Connected to DB!")
 });
 
-const db = mongoose.connection;
-
 autoIncrement.initialize(db);
 
 const bugSchema = new mongoose.Schema({
-    bugName: '1',
-    bugDescription: 'Async Swim needs to be fixed, desperately.',
-    reportedBy: 'Bailey',
-    createdDate: '1/5/2020',
-    assignedTo: 'Teddi',
-    threatLevel: 'Critical',
+    bugName: mongoose.Types.ObjectId,
+    bugDescription: String,
+    reportedBy: String,
+    createdDate: {type: Date, default: Date.now},
+    assignedTo: String,
+    threatLevel: String,
 })
 
 bugSchema.plugin(autoIncrement.plugin, 'Bug');
-const Bug = db.model('Bug', bugSchema);
+const Bug = db.model('Bug', bugSchema, 'bugs');
+
+newBug = (bug) => {
+    const newBug = new Bug(bug);
+    return newBug.save()
+    .then(results => results)
+    .catch(err => {throw err})
+}
+viewAllBugs = () => {
+    return Bug.find()
+    .then (results => results)
+    .catch (err => {throw err})
+}
+
+module.exports = {newBug, viewAllBugs}
--- server/index.js
+++ server/index.js
@@ -2,16 +2,21 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = 3000;
+const database = require('./db/queries');
 
 app.use(bodyParser.json())
 
 app.get('/bugs', (req,res)=>{
     console.log("bugs requested");
-    res.sendStatus(200);
+    database.viewAllBugs()
+    .then (result => res.send(result))
+    .catch (err => {throw err})
 });
 app.post('/bugs', (req, res)=>{
     console.log("Bug post received");
-    res.sendStatus(200);
+    database.newBug(req.body)
+    .then(result => res.send(result))
+    .catch(err => {throw err})
 });
 
 //server.listen
